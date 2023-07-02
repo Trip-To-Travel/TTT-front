540c826255c4ef1f3f8ea8af6f1b7712c1e3989b
yewon0804 <ehdnjs0804@gmail.com>
2023-07-03 04:11:19 +0900

    design: '내 정보' 최상단 app bar 추가 (#14)

--- lib/screens/mypage/myInfo.dart
+++ lib/screens/mypage/myInfo.dart
@@ -6,6 +6,15 @@ class MyInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back),
+          ),
+          title: Text("내 정보", style: TextStyle(color: Colors.black, fontSize: 16),),
+        ),
         body: SafeArea(
           child: Container(
             child: Text("내 정보"),
