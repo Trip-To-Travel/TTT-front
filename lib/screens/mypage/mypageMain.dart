b15f68518c472799a576993a7c718edd28d2a39a
yewon0804 <ehdnjs0804@gmail.com>
2023-07-03 23:16:58 +0900

    design: 마이페이지 메뉴 하나가 한 줄을 차지하도록, 레이아웃 변경 (#12)

--- lib/screens/mypage/mypageMain.dart
+++ lib/screens/mypage/mypageMain.dart
@@ -26,7 +26,6 @@ class _MyPageState extends State<MyPage> {
         body: SafeArea(
           child: Container(
               color: Color(0xffebebeb),
-              // padding: EdgeInsets.fromLTRB(0, 4, 0, 4),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween, // 추후 메뉴 추가로 인해 스크롤 메뉴가 될 시, 해당 속성은 불필요 할 듯
                 children: [
@@ -35,13 +34,6 @@ class _MyPageState extends State<MyPage> {
                       Container( // 프로필 사진, 프로필 정보(닉네임)
                         color: Colors.white,
                         padding: EdgeInsets.all(10),
-                        // decoration: BoxDecoration(
-                        //     color: Colors.white,
-                        //   border: Border.all(
-                        //     color: Colors.white,
-                        //   ),
-                        //   borderRadius: BorderRadius.all(Radius.circular(4))
-                        // ),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
@@ -104,7 +96,7 @@ class _MyPageState extends State<MyPage> {
                       Column( // 안내: 연말정산, 공지사항, 자주하는 질문, 1:1 문의
                         children: [
                           Container( // 타이틀
-                            margin: EdgeInsets.fromLTRB(16, 8, 16, 2),
+                            margin: EdgeInsets.fromLTRB(16, 16, 16, 2),
                             alignment: Alignment.bottomLeft,
                             child: Text(
                               "안내",
@@ -113,105 +105,116 @@ class _MyPageState extends State<MyPage> {
                           Container( // 메뉴들 수납 공간
                             color: Colors.white,
                             padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            child: Column(
                               children: [
                                 Container( // 메뉴: 안내 > 연말정산
+                                  padding: EdgeInsets.fromLTRB(20, 16, 20, 16),
                                   child: InkWell(
                                     onTap: () {
                                       print("연말정산 페이지로 이동");
                                     },
-                                    child: Column(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                       children: [
-                                        Container(
-                                            margin: EdgeInsets.all(6),
-                                            child: Icon(
-                                              Icons.airplane_ticket_sharp,
-                                              color: Colors.grey,
+                                        Row(
+                                          children: [
+                                            Container(
+                                                margin: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                                                child: Icon(
+                                                  Icons.airplane_ticket_sharp,
+                                                  color: Colors.grey,
+                                                )
+                                            ),
+                                            Text(
+                                              "연말정산",
                                             )
+                                          ],
                                         ),
-                                        Text(
-                                          "연말정산",
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color: Colors.grey // -> 연말정산 업데이트 되었을 때는, ttt 테마 색상으로 깜빡이게 하면 어떨까?
-                                          ),
-                                        )
+                                        Icon(Icons.arrow_forward_ios_sharp, size: 16)
                                       ],
                                     ),
                                   ),
                                 ),
                                 Container( // 메뉴: 안내 > 공지사항
+                                  padding: EdgeInsets.fromLTRB(20, 16, 20, 16),
                                   child: InkWell(
                                     onTap: () {
                                       print("공지사항 페이지로 이동");
                                     },
-                                    child: Column(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                       children: [
-                                        Container(
-                                            margin: EdgeInsets.all(6),
-                                            child: Icon(
-                                              Icons.announcement_sharp,
-                                              color: Colors.grey,
+                                        Row(
+                                          children: [
+                                            Container(
+                                                margin: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                                                child: Icon(
+                                                  Icons.announcement_sharp,
+                                                  color: Colors.grey,
+                                                )
+                                            ),
+                                            Text(
+                                              "공지사항",
                                             )
+                                          ],
                                         ),
-                                        Text(
-                                          "공지사항",
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color: Colors.grey
-                                          ),
-                                        )
+                                        Icon(Icons.arrow_forward_ios_sharp, size: 16)
                                       ],
                                     ),
                                   ),
                                 ),
                                 Container( // 메뉴: 안내 > 자주하는 질문
+                                  padding: EdgeInsets.fromLTRB(20, 16, 20, 16),
                                   child: InkWell(
                                     onTap: () {
                                       print("자주하는 질문 페이지로 이동");
                                     },
-                                    child: Column(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                       children: [
-                                        Container(
-                                            margin: EdgeInsets.all(6),
-                                            child: Icon(
-                                              Icons.question_mark_sharp,
-                                              color: Colors.grey,
+                                        Row(
+                                          children: [
+                                            Container(
+                                                margin: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                                                child: Icon(
+                                                  Icons.question_mark_sharp,
+                                                  color: Colors.grey,
+                                                )
+                                            ),
+                                            Text(
+                                              "자주하는 질문",
                                             )
+                                          ],
                                         ),
-                                        Text(
-                                          "자주하는 질문",
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color: Colors.grey
-                                          ),
-                                        )
+                                        Icon(Icons.arrow_forward_ios_sharp, size: 16)
                                       ],
                                     ),
                                   ),
                                 ),
                                 Container( // 메뉴: 안내 > 1:1 문의
+                                  padding: EdgeInsets.fromLTRB(20, 16, 20, 16),
                                   child: InkWell(
                                     onTap: () {
                                       print("1:1 문의 페이지로 이동");
                                     },
-                                    child: Column(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                       children: [
-                                        Container(
-                                            margin: EdgeInsets.all(6),
-                                            child: Icon(
-                                              Icons.question_answer_sharp,
-                                              color: Colors.grey,
+                                        Row(
+                                          children: [
+                                            Container(
+                                                margin: EdgeInsets.fromLTRB(0, 0, 16, 0),
+                                                child: Icon(
+                                                  Icons.question_answer_sharp,
+                                                  color: Colors.grey,
+                                                )
+                                            ),
+                                            Text(
+                                              "1:1 문의",
                                             )
+                                          ],
                                         ),
-                                        Text(
-                                          "1:1 문의",
-                                          style: TextStyle(
-                                              fontSize: 12,
-                                              color: Colors.grey
-                                          ),
-                                        )
+                                        Icon(Icons.arrow_forward_ios_sharp, size: 16)
                                       ],
                                     ),
                                   ),
