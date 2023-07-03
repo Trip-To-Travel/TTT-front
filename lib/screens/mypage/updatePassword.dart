6088698f294de6af248d52df36c8ea4fdf5f0969
yewon0804 <ehdnjs0804@gmail.com>
2023-07-04 04:10:19 +0900

    design: '비밀번호 변경' 페이지 UI 초안 작성 (#18)

--- lib/screens/mypage/updatePassword.dart
+++ lib/screens/mypage/updatePassword.dart
@@ -11,16 +11,81 @@ class UpdatePassword extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
         ),
-        title: Text("비밀번호 변경", style: TextStyle(color: Colors.black, fontSize: 16),),
+        title: const Text("비밀번호 변경", style: TextStyle(color: Colors.black, fontSize: 16),),
       ),
       body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              Text("비밀번호 변경")
-            ],
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff76BDFF),
+                      )
+                  ),
+                  labelText: "비밀번호"
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff76BDFF),
+                      )
+                    ),
+                    labelText: "비밀번호 확인"
+                ),
+              ),
+            ),
+            const BtnUpdate() // 변경 버튼
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BtnUpdate extends StatelessWidget {
+  const BtnUpdate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Ink(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            color: Color(0xff76BDFF),
+            borderRadius: BorderRadius.all(
+                Radius.circular(5)
+            )
+        ),
+        child: InkWell(
+          onTap: () {
+            print("비밀번호 변경 POST 요청");
+          },
+          borderRadius: const BorderRadius.all(
+              Radius.circular(5)
+          ),
+          child: Container(
+              padding: const EdgeInsets.fromLTRB(0, 16, 0, 16),
+              child: const Text(
+                "변경",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              )
           ),
         ),
       ),
