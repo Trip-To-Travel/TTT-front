a9c91e9bdca7b38a0838711c18c9551884a02ccc
yewon0804 <ehdnjs0804@gmail.com>
2023-07-03 22:24:22 +0900

    design: 내 정보 UI 초안 작성 (#14)

--- lib/screens/mypage/myInfo.dart
+++ lib/screens/mypage/myInfo.dart
@@ -17,7 +17,79 @@ class MyInfo extends StatelessWidget {
         ),
         body: SafeArea(
           child: Container(
-            child: Text("내 정보"),
+            child: Column(
+              children: [
+                Container( // 현재의 '내 정보'들: 아이디, 닉네임, 이메일
+                  padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
+                  color: Color(0xffebebeb),
+                  child: Column(
+                    children: [
+                      Row( // 아이디
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("아이디"),
+                          Text("testuser"),
+                        ],
+                      ),
+                      Row( // 닉네임
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("닉네임"),
+                          Text("티티티닉네임"),
+                        ],
+                      ),
+                      Row( // 이메일
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("이메일"),
+                          Text("email@email.com"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container( // 메뉴
+                  child: Column(
+                    children: [
+                      Container( // 비밀번호 변경
+                        child: InkWell(
+                          onTap: () {
+                            print("비밀번호 변경 페이지로 이동");
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("비밀번호 변경"),
+                                Icon(Icons.arrow_forward_ios_sharp, size: 16)
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container( // 회원정보 변경
+                        child: InkWell(
+                          onTap: () {
+                            print("회원정보 변경 페이지로 이동");
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(20),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("회원정보 변경"),
+                                Icon(Icons.arrow_forward_ios_sharp, size: 16)
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
           ),
         )
     );
